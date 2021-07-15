1c154a4adb269a3fd5082f661136a20e1eabef6a
phumin-psu <6210110272@psu.ac.th>
2021-07-15 11:03:39 +0700

    api

--- components/Weather.js
+++ components/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, ImageBackground, StyleSheet, Text } from 'react-native';
 import Forecast from './Forecast';
 
@@ -8,6 +8,24 @@ export default function Weather(props) {
         description: 'description',
         temp: 0
     })
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=d4e2ffa59d2003bd57fc24d1c5277177`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp
+                });
+            })
+            .catch((error) => {
+                console.warn(error);
+            });
+        }
+    }, [props.zipCode])
 
     return (
         <View>
