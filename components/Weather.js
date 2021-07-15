41bb9d083f8bfcbbedcb87f908d92619cbd1bbcb
phumin-psu <6210110272@psu.ac.th>
2021-07-15 09:53:27 +0700

    set weather

--- components/Weather.js
+++ components/Weather.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { ImageBackground, StyleSheet, Text } from 'react-native';
+import Forecast from './Forecast';
 
 export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+    })
+
     return (
-        <Text>{props.zipCode}</Text>
+        <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+            <Text>Zip Code</Text>
+            <Text>{props.zipCode}</Text>
+            <Forecast {...forecastInfo} />
+        </ImageBackground>
     );
 }
-   
+
+const styles = StyleSheet.create({
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    },
+});
+
