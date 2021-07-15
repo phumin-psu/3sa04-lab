3f4a6c557a0971496486220fa76f6a44bdf2a6bd
phumin-psu <6210110272@psu.ac.th>
2021-07-15 10:03:00 +0700

    remove style center

--- components/Weather.js
+++ components/Weather.js
@@ -20,7 +20,7 @@ export default function Weather(props) {
 
 const styles = StyleSheet.create({
     backdrop: {
-        alignItems: 'center',
+        
         width: '100%',
         height: '100%'
     },
